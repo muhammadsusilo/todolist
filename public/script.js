e665d89b149e441061fa26cc576161e39e081c03
Muhammad Susilo <muhammadsusilo43@gmail.com>
2023-12-24 15:12:20 +0700

    menyempurnakan 3 file tinggal memperbaiki file cssnya agar lebih menarik supaya dilihat

--- public/script.js
+++ public/script.js
@@ -3,5 +3,42 @@ const button = document.getElementById("btn");
 button.addEventListener("click", function() {
     if (document.querySelector("#wrapper input").value.length == 0) {
         alert("Todo List tidak boleh kosong")
+    } else {
+        document.getElementById("task").innerHTML +=
+            `<div class="tasks">
+                <span id="taskname">
+                 ${document.querySelector('#wrapper input').value}
+                </span>
+                <button class="delete">
+                <i class='bx bx-message-square-x'></i>
+                </button>
+             </div>
+             `;
     }
-})
+
+    const buttonDelete = document.querySelectorAll(".delete");
+
+    for (let i = 0; i < buttonDelete.length; i++) {
+
+        // const element = array[index];
+        // buttonDelete[i].onclick = function() {
+        //         this.parentNode.remove();
+        //     }
+
+        buttonDelete[i].addEventListener("click", function() {
+            this.parentNode.remove();
+        })
+    }
+
+    const tasks = document.querySelectorAll(".tasks");
+
+    for (let a = 0; a < tasks.length; a++) {
+        tasks[a].addEventListener("click", function() {
+            this.classList.toggle("complete");
+        })
+    }
+
+    document.querySelector("#wrapper input").value = "";
+
+
+});
